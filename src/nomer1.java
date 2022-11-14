18284f4eb8c4d9050a2a4146cb721e1806fe1456
Nizar2710 <bladescoter5@gmail.com>
2022-11-14 19:33:40 +0700

    Initial commit

---
+++ src/nomer1.java
@@ -0,0 +1,15 @@
+public class nomer1 {
+    // Jika tipe Data float dan double di Kalikan Maka Hasilnya akan Muncul
+    // karena flot turunan dari doubel
+    // tipe data Dqouble merupakan
+    public static void main(String[] args){
+
+        float  var1;
+        double var2;
+
+        var1 = 21.98f;
+        var2 = 2.56;
+
+        System.out.println(" Hasil : " + var1 * var2);
+    }
+}
---
+++ src/nomer2.java
@@ -0,0 +1,12 @@
+public class nomer2 {
+    public static void main(String[] args) {
+        float[] myNumber = new float[] {2.34f,4.56f,67.89f};
+        PembulatanKeAtas(myNumber);
+        }
+        public static void PembulatanKeAtas(float[]myNumber){
+            System.out.println();
+
+        }
+
+
+    }
